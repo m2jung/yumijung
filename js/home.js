dd86a9c6a3302b216f16a19276315cdc97846916
유미정 Yu Mijung <mee607@hanmail.net>
2023-08-08 22:20:33 +0900

    first commit

--- js/home.js
+++ js/home.js
@@ -1,9 +1,10 @@
 const wrapper = document.querySelector(".wrapper");
 const nav = document.querySelector('nav');
-
+const container = document.querySelector('div.container');
 // 페이지 로드시 실행 
 window.onload = function () {
     wrapper.style.opacity = 1;
-    // nav에 'show' 클래스를 추가 animation
+    // 요소에 'show' 클래스를 추가 animation
     nav.classList.add('show');
+    container.classList.add('show');
   }
